53808b6b5141bc152f6949fd2cceaf3f172dd39c
Viktoriia <viktoriamyhailiak@gmail.com>
2025-06-01 16:01:36 +0300

    add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,30 +7,33 @@ const array = [];
 
 for (const country of population) {
   array.push(
-    +country.innerHTML
+    country.innerHTML
       .split('')
       .filter((e) => e !== ',')
       .join(''),
   );
 }
 
-let total = array.reduce((prev, elem) => prev + elem, 0).toString();
-let average = Math.round(total / array.length).toString();
+let total = array.reduce((prev, elem) => prev + Number(elem), 0);
+let average = Math.round(total / array.length);
 const result = [total, average];
-const processedValues = [];
 
-for (let number of result) {
-  for (let i = number.length - 3; i >= 1; i -= 3) {
-    const arr = number.split('');
+const processedValues = result.map((number) => {
+  let str = number.toString();
+
+  for (let i = str.length - 3; i > 0; i -= 3) {
+    const arr = str.split('');
 
     arr.splice(i, 0, ',');
-    number = arr.join('');
+    str = arr.join('');
   }
-  processedValues.push(number);
-}
+
+  return str;
+});
 
 total = processedValues[0];
 average = processedValues[1];
 
 totalElement.innerHTML = total;
 averageElement.innerHTML = average;
+
